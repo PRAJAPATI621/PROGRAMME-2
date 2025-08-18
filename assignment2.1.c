6c4efc7dd4df01cda2d4551746e01db328be655b
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-18 20:01:18 +0530

    Add files via upload

---
+++ assignment2.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter 2 number\n");
+ scanf("%d %d",&a,&b);
+ f(a>b)
+ {
+  ptintf("%d is the larger number and %d is the smaller number among the number",a,b);
+ }
+ else
+ {
+  printf("both numbers are equal",a,b);
+ }
+}
