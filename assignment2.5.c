a504706a62a03d68354998e3515a6db0d3554461
PRAJAPATI621 <mahiprajapati20308@gmail.com>
2025-08-21 22:19:35 +0530

    Add files via upload

---
+++ assignment2.5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+ int a;
+ printf("Enter gross sales");
+ scanf("%d",&a);
+ if(a>20000)
+ {
+  printf("%d - %d*0.1");
+ }
+ if(a>10000)
+ {
+  printf("%d - %d*0.15");
+ }
+ else
+ {
+  printf("%d - %d*0.05");
+ }
+ return 0;
+}
